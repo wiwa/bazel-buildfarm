58053b64f3b267f48f8f51b158e8c25bc379ccdb
Win Wang <fallenpwr@gmail.com>
2022-04-08 00:59:15 -0400

    wip

---
+++ persistentworkers/src/main/java/persistent/ProcessWrapper.java
@@ -0,0 +1,52 @@
+package persistent;
+
+import com.google.common.collect.ImmutableList;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+public class ProcessWrapper {
+    
+    private final ImmutableList<String> args;
+    
+    private final Process process;
+    
+    private final Path workDir;
+    
+    private final Path stdErrFile;
+    
+    public ProcessWrapper(Path workDir, Path stdErrFile, String[] args) throws IOException {
+        this.args = ImmutableList.copyOf(args);
+        this.workDir = workDir;
+        this.stdErrFile = stdErrFile;
+        
+        ProcessBuilder pb = new ProcessBuilder()
+                .command(this.args)
+                .directory(this.workDir.toFile())
+                .redirectError(stdErrFile.toFile());
+        
+        this.process = pb.start();
+    }
+    
+    public ImmutableList<String> getInitialArgs() {
+        return this.args;
+    }
+    
+    public Path getWorkDir() {
+        return this.workDir;
+    }
+    
+    public OutputStream getStdIn() {
+        return this.process.getOutputStream();
+    }
+
+    public InputStream getStdOut() {
+        return this.process.getInputStream();
+    }
+    
+    public Path getStdErrPath() {
+        return this.stdErrFile;
+    }
+}
